58b2ddbbe71ab33f091971445b104f713814410f
Robert Rodriguez <robertrodriguez@MacBook-Pro-de-Cris.local>
2023-12-25 11:35:15 +0100

    juego 2

--- ejerciciosJs/games/exercise-2.js
+++ ejerciciosJs/games/exercise-2.js
@@ -1,3 +1,37 @@
+let posiciones = document.querySelectorAll(".b-square");
+let puntuaciondiv = document.querySelector(`[data-function*="score"]`);
+let tiempoDiv = document.querySelector(`[data-function*="time-left"]`);
+const diggletimg = document.createElement("img");
+diggletimg.setAttribute("src", "./public/exercise-2/mole.png");
+diggletimg.setAttribute("class", "diggletimg");
+let puntuacionStart = 0;
+
+const sumarpunto = () => {
+  puntuaciondiv.textContent = puntuacionStart++;
+};
+diggletimg.addEventListener("click", sumarpunto);
+
+const moveDigglet = () => {
+  let numero = Math.floor(Math.random() * posiciones.length);
+  posiciones[numero].appendChild(diggletimg);
+};
 
 
+const crono = () => {
+    tiempo = tiempoDiv.innerText -1;
+    tiempoDiv.innerText = tiempo;
+    console.log(tiempo)
+    if (tiempo == 0 ){
+        
+    tiempoDiv.innerText = 30;
+        
+        alert(puntuaciondiv.innerText)
 
+    }
+
+};
+
+  setInterval(moveDigglet, 500);
+  setInterval(crono, 1000)
+
+console.log(tiempoDiv.innerText);
